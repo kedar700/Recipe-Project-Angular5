c0d3c6b1bb646ddc676d733476935548f5edd37a
Kedar Naik <knaik@fhlbdm.com>
2018-02-11 12:59:28 -0600

    Added basic funtionality for navigation.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -10,12 +10,16 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Another Test Recipe', 'This is simply a test',
+'https://therealfoodrds.com/wp-content/uploads/2017/07/Chicken-Tikka-Masala-Skewers-012-e1501011368257.jpg')
   ];
-
+  @Output() recipeListSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected(recipe: Recipe): void {
+  this.recipeListSelected.emit(recipe);
+  }
 }
